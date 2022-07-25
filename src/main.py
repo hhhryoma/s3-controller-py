8273ba412124813453c1b0c9f69afb711a74c728
ryouma.hishida <poipoi.rh@gmail.com>
2022-07-25 22:22:51 +0900

    get_object追加

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
 import module
 
 bucket = "python-work"
+key = "sample.zip"
 
-for i in range(3):
-    file_name = module.make_text_file("file_" + str(i))
-    result = module.upload_file(file_name=file_name, bucket=bucket, object_name=None)
-    if result:
-        print(f'{file_name} upload Succcess')
-    else:
-        print('upload failed')
+file_name = "sample.zip"
+
+module.upload_file(file_name=file_name, bucket=bucket, object_name=key)
+module.get_object(bucket, key)
--- src/module.py
+++ src/module.py
@@ -1,38 +1,85 @@
+import hashlib
+import zipfile
+import io
 import boto3
 import botocore
 import os
 
 def upload_file(file_name, bucket, object_name=None):
-    """ s3へのファイルアップロード時のAPIとして1,upload_file/2.put_objectの2種類ある
-        双方のAPIの仕様差異については右記参照（https://stackoverflow.com/questions/43739415/what-is-the-difference-between-file-upload-and-put-object-when-uploading-fil）
-    Args:
-        file_name (_type_): _description_
-        bucket (_type_): _description_
-        object_name (_type_, optional): _description_. Defaults to None.
-    """
+    # s3へのファイルアップロード時のAPIとして1,upload_file/2.put_objectの2種類ある
+    # 双方のAPIの仕様差異については右記参照（https://stackoverflow.com/questions/43739415/what-is-the-difference-between-file-upload-and-put-object-when-uploading-fil）
+
     if object_name is None:
         object_name = os.path.basename(file_name)
 
     # boto3.set_stream_logger()
     # botocore.session.Session().set_debug_logger()
-    with open(file_name, 'rb') as f:
+    with open(ret_file_path(file_name), 'rb') as f:
         s3 = boto3.client('s3')
         response = s3.put_object(
             Body=f,
             Bucket=bucket,
             Key=object_name
         )
+
+    print("\n--------------------- upload response ---------------------")
     print(response)
-    return response['ResponseMetadata']['HTTPStatusCode'] == 200
+    print("--------------------- upload response ---------------------\n")
+
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        print(f'{file_name} upload Succcess')
+    else:
+        print('upload failed')
+
+
+def get_object(bucket, key):    
+    # S3バケットからファイルを取得、ファイル内容をコンソール出力
+
+    s3 = boto3.client('s3')
+    response = s3.get_object(
+        Bucket=bucket,
+        Key=key
+    )
     
+    print("\n--------------------- get object response ---------------------")
+    print(response)
+    print("--------------------- get object response ---------------------\n")
+    
+    obj = response['Body'].read()
+    # メモリにZipファイルを保存
+    zf = zipfile.ZipFile(io.BytesIO(obj))
+    file = io.TextIOWrapper(zf.open(zf.namelist()[0], 'r'))
+    
+    print('get object content:')
+    for row in file.readlines():
+        print(row)
+    get_hash('sha256', file)
     
 
+
 def make_text_file(file_name):
-    base_path = os.path.dirname(os.path.abspath(__file__))
-    path = os.path.join(base_path, "up", file_name)
-    print(os.curdir)
+    path = ret_file_path(file_name)
     f = open(path, 'w')
     f.write('auto generate')
     f.close()
     return path
 
+
+def ret_file_path(file_name):
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(base_path, 'up', file_name)
+    return path
+
+
+def get_hash(algo, file):
+    file_data = file.read()
+    if algo == 'sha256':
+        hash = hashlib.sha256(file_data.encode()).hexdigest() 
+        # hash = hashlib.sha256(file_byte).hexdigest() 
+    print(f'hash algo: {algo}   hash string: {hash}')
+    return hash
+
+
+def wrap_log_text(log_text, wrap_text):
+    l = [wrap_text, log_text, wrap_text]
+    return '\n'.join(l)
